7707e0341d8ef22fdfd436621fb45f33dfbf336d
mostafaa-khattab <mostafakhattab1555@gmail.com>
2024-10-07 08:45:35 +0300

    connect db

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
 
-    return await mongoose.connect(process.env.DB_LOCAL)
+    return await mongoose.connect(process.env.DB_LOCAL || "mongodb+srv://admin:admin@cluster0.yfj3znp.mongodb.net/codeavour")
         .then(res => console.log(`DB Connected successfully on .........`))
         .catch(err => console.log(` Fail to connect  DB.........${err} `))
 }
